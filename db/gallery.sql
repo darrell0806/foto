1cf9acfed1750412bf3105a0c6f6c6b6a614d073
unknown <darrell260806@gmail.com>
2024-01-24 11:17:14 +0700

    Darrell

--- db/gallery.sql
+++ db/gallery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Jan 2024 pada 17.00
+-- Waktu pembuatan: 24 Jan 2024 pada 05.12
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 7.4.29
 
@@ -29,7 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `album` (
   `id_album` int(11) NOT NULL,
+  `cover` text NOT NULL,
   `nama_album` text NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -37,9 +39,62 @@ CREATE TABLE `album` (
 -- Dumping data untuk tabel `album`
 --
 
-INSERT INTO `album` (`id_album`, `nama_album`, `created_at`) VALUES
-(1, 'Makanan', '2024-01-22 21:04:23'),
-(2, 'Ruangan', '2024-01-22 15:04:43');
+INSERT INTO `album` (`id_album`, `cover`, `nama_album`, `user_id`, `created_at`) VALUES
+(1, '1705932231_5bb9b8779ce3c811f65a.jpg', 'Makanan', 1, '2024-01-22 21:04:23'),
+(2, '1705932358_615ab0b921783a7d7cf9.jpg', 'Ruangan', 1, '2024-01-22 15:04:43'),
+(4, '1706025748_7fe4aa56786743aacd27.jpg', 'Buku', 2, '2024-01-23 10:02:28');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `comment`) VALUES
+(1, 1, 1, 'aaa'),
+(2, 1, 1, 'pe'),
+(3, 2, 1, 'w'),
+(4, 1, 1, 'Bagus'),
+(5, 4, 1, 'Bagus la'),
+(6, 1, 1, 'halo'),
+(7, 4, 1, 'Sedap');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `likes`
+--
+
+INSERT INTO `likes` (`id`, `post_id`, `user_id`, `status`) VALUES
+(8, 5, 1, ''),
+(9, 4, 1, ''),
+(10, 1, 1, 'Like'),
+(11, 6, 1, 'Like'),
+(12, 3, 1, 'Like'),
+(13, 2, 1, 'Like'),
+(14, 1, 2, 'Like');
 
 -- --------------------------------------------------------
 
@@ -62,10 +117,10 @@ CREATE TABLE `post` (
 
 INSERT INTO `post` (`id_post`, `fotop`, `deskripsi`, `user_maker`, `album`, `created_at`) VALUES
 (1, '1705932231_5bb9b8779ce3c811f65a.jpg', 'Curry Puff', 1, 1, '2024-01-22 08:03:51'),
-(2, '1705932358_615ab0b921783a7d7cf9.jpg', 'Hotel', 1, 2, '2024-01-22 08:05:58'),
 (3, '1705932440_9b51ab3c4ebaaf04133e.jpg', 'Kantin', 1, 2, '2024-01-22 08:07:20'),
-(4, '1705932474_f4366a4eca33b9a9574d.jpg', 'Kwasong', 1, 1, '2024-01-22 08:07:54'),
-(5, '1705938221_18728349c043e8da2800.jpg', 'Kari Pap', 1, 1, '2024-01-22 09:43:41');
+(4, '1705932474_f4366a4eca33b9a9574d.jpg', 'Kwasong', 2, 1, '2024-01-22 08:07:54'),
+(5, '1705938221_18728349c043e8da2800.jpg', 'Kari Pap', 1, 1, '2024-01-22 09:43:41'),
+(6, '1706025623_721927999a60e27320fb.jpg', 'Dining Room', 1, 2, '2024-01-23 10:00:23');
 
 -- --------------------------------------------------------
 
@@ -86,8 +141,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama`, `foto`, `username`, `password`) VALUES
-(1, 'Darrell', 'default.jpg', 'dar', '0cc175b9c0f1b6a831c399e269772661'),
-(2, 'a', 'default.jpg', 'a', '0cc175b9c0f1b6a831c399e269772661');
+(1, 'Darrell', 'tidaktahu.png', 'dar', '0cc175b9c0f1b6a831c399e269772661'),
+(2, 'Kevin', 'default.jpg', 'kev', '0cc175b9c0f1b6a831c399e269772661');
 
 --
 -- Indexes for dumped tables
@@ -98,6 +153,18 @@ INSERT INTO `user` (`id_user`, `nama`, `foto`, `username`, `password`) VALUES
 --
 ALTER TABLE `album`
   ADD PRIMARY KEY (`id_album`);
+
+--
+-- Indeks untuk tabel `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `post`
@@ -119,13 +186,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `album`
 --
 ALTER TABLE `album`
-  MODIFY `id_album` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_album` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `post`
 --
 ALTER TABLE `post`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
